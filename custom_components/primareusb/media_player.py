0c45a5eff575bedf5022eefae79a90ec96972650
Jon Davidsson <jon@birdinatree.se>
2023-02-13 10:14:03 +0100

    Changes

--- custom_components/primareusb/media_player.py
+++ custom_components/primareusb/media_player.py
@@ -14,7 +14,7 @@ from homeassistant.components.media_player import (
 from homeassistant.components.media_player.const import (
     SUPPORT_VOLUME_SET,
     SUPPORT_VOLUME_MUTE, SUPPORT_TURN_ON, SUPPORT_TURN_OFF,
-    SUPPORT_VOLUME_STEP, SUPPORT_SELECT_SOURCE, SUPPORT_SELECT_SOUND_MODE
+    SUPPORT_VOLUME_STEP
 )
 from homeassistant.const import (
     CONF_NAME, STATE_OFF, STATE_ON)
@@ -22,29 +22,24 @@ import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
-_LOGGER.debug("Loading Primare platform")
+_LOGGER.debug("Loading Primare platform 1.0.0")
 
 DEFAULT_NAME = 'Primare Receiver'
 DEFAULT_MIN_VOLUME = 0
 DEFAULT_MAX_VOLUME = 79
 
 SUPPORT_MARANTZ = SUPPORT_VOLUME_SET | SUPPORT_VOLUME_MUTE | \
-    SUPPORT_TURN_ON | SUPPORT_TURN_OFF | SUPPORT_VOLUME_STEP | \
-    SUPPORT_SELECT_SOURCE | SUPPORT_SELECT_SOUND_MODE 
+    SUPPORT_TURN_ON | SUPPORT_TURN_OFF | SUPPORT_VOLUME_STEP 
 
 CONF_SERIAL_PORT = 'serial_port'
 CONF_MIN_VOLUME = 'min_volume'
 CONF_MAX_VOLUME = 'max_volume'
-CONF_SOURCE_DICT = 'sources'
-CONF_SOUNDMODE_DICT = 'soundmode'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_SERIAL_PORT): cv.string,
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
     vol.Optional(CONF_MIN_VOLUME, default=DEFAULT_MIN_VOLUME): int,
     vol.Optional(CONF_MAX_VOLUME, default=DEFAULT_MAX_VOLUME): int,
-    vol.Optional(CONF_SOURCE_DICT, default={}): {cv.string: cv.string},
-    vol.Optional(CONF_SOUNDMODE_DICT, default={}): {cv.string: cv.string},
 })
 
 
@@ -56,29 +51,20 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         PrimareReceiver(config.get(CONF_SERIAL_PORT)),
         config.get(CONF_MIN_VOLUME),
         config.get(CONF_MAX_VOLUME),
-        config.get(CONF_SOURCE_DICT),
-        config.get(CONF_SOUNDMODE_DICT)
     )], True)
 
 
 class Primare(MediaPlayerEntity):
     """Representation of a Primare Receiver."""
 
-    def __init__(self, name, primare_preamp, min_volume, max_volume,
-                 source_dict, sound_mode_dict):
+    def __init__(self, name, primare_preamp, min_volume, max_volume):
         """Initialize the Primare Receiver device."""
         self._name = name
         self._primare_receiver = primare_preamp
         self._min_volume = min_volume
         self._max_volume = max_volume
-        self._source_dict = source_dict
-        self._sound_mode_dict = sound_mode_dict
-        self._reverse_mapping = {value: key for key, value in
-                                 self._source_dict.items()}
-        self._reverse_mapping_sound_mode = {value: "0{}".format(key) for key, value in
-                                 self._sound_mode_dict.items()}
 
-        self._volume = self._state = self._mute = self._source = None
+        self._volume = self._state = self._mute = None
 
     def calc_volume(self, decibel):
         """
@@ -123,11 +109,6 @@ class Primare(MediaPlayerEntity):
         volume_result = self._primare_receiver.main_volume('W', 'R')
         if (volume_result != None):
             self._volume = self.calc_volume(volume_result)
-            
-        self._source = self._source_dict.get(
-            self._primare_receiver.main_source('W', 'R'))
-        self._sound_mode = self._sound_mode_dict.get(
-            self._primare_receiver.main_sound_mode('W', 'R'))
 
     @property
     def volume_level(self):
@@ -146,32 +127,26 @@ class Primare(MediaPlayerEntity):
 
     def turn_off(self):
         """Turn the media player off."""
-        self._primare_receiver.main_power('W', '3')
+        self._primare_receiver.main_power('W', '\x00')
 
     def turn_on(self):
         """Turn the media player on."""
-        self._primare_receiver.main_power('W', '2')
+        self._primare_receiver.main_power('W', '\x01')
 
     def volume_up(self):
         """Volume up the media player."""
-        self._primare_receiver.main_volume('W', '1')
+        _LOGGER.debug("Clicked volume up")
+        self._primare_receiver.main_volume('W', '\x01')
 
     def volume_down(self):
         """Volume down the media player."""
-        self._primare_receiver.main_volume('W', '2')
+        _LOGGER.debug("Clicked volume down")
+        self._primare_receiver.main_volume('W', '\xff')
 
     def set_volume_level(self, volume):
         """Set volume level, range 0..1."""
         vol_calc = '0' + str(self.calc_db(volume))
         self._primare_receiver.main_volume('W', vol_calc)
-
-    def select_source(self, source):
-        """Select input source."""
-        self._primare_receiver.main_source('W', self._reverse_mapping.get(source))
-
-    def select_sound_mode(self, sound_mode):
-        """Select sound mode."""
-        self._primare_receiver.main_sound_mode('W', self._reverse_mapping_sound_mode.get(sound_mode))
         
     def mute_volume(self, mute):
         """Mute (true) or unmute (false) media player."""
@@ -180,23 +155,3 @@ class Primare(MediaPlayerEntity):
             self._primare_receiver.main_mute('W', '\x01')
         else:
             self._primare_receiver.main_mute('W', '\x00')
-            
-    @property
-    def source(self):
-        """Name of the current input source."""
-        return self._source
-
-    @property
-    def sound_mode(self):
-        """Name of the current sound_mode."""
-        return self._sound_mode
-
-    @property
-    def source_list(self):
-        """List of available input sources."""
-        return sorted(list(self._reverse_mapping.keys()))
-
-    @property
-    def sound_mode_list(self):
-        """List of available sound_modes."""
-        return sorted(list(self._reverse_mapping_sound_mode.keys()))
